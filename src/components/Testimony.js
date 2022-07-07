3dedeeef2e907298057dc62ed9722975bbc6a15c
Reeyna <reeynarl@gmail.com>
2022-07-07 12:46:38 -0500

    [ADD] values for components

--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React from 'react';
+import '../css/Testimony.css';
 
-function Testimony () {
+function Testimony (props) {
   return (
-    <div className="contenedor-testimonio">
-    
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio' 
+        src={require(`../img/testimonio-${props.imagen}.png`)}
+        alt='Foto de Emma' />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
     </div>
   )
 }
+
+export default Testimony;
